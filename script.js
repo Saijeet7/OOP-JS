648ad2d2b2f0a65584d8f0ba1ea5728b30f53507
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-06-27 22:23:06 +0545

    prototype

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+const jonas = new Person("Jonas", 1991);
+const matilda = new Person("Matilda", 2015);
+
+// protoypes
+Person.prototype.calcAge = function () {
+  console.log(2024 - this.birthYear);
+};
+
+jonas.calcAge();
